ffd1f4af7becbef7ab3a8ecc3e151c6a6defc2a9
Joy <joytran10899@gmail.com>
2025-04-29 17:44:25 -0700

    Tutorial View

---
+++ EM385(2)/Controller/PageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PageViewController.swift
+//  EM385(2)
+//
+//  Created by Joy on 4/11/25.
+//
+
+import UIKit
+
+class PageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
